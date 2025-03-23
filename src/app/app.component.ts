0d242a9818ea0579f080fdf48de4b9564fee4419
Saranga Ranasinghe <ranasingherpksb.22@uom.lk>
2025-03-23 22:51:49 +0530

    frontend
    frontend completed 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "./healthOfficer/sidebar/sidebar.component";
+import { SignInFormComponent } from './healthOfficer/sign-in-form/sign-in-form.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SidebarComponent,SignInFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Sports-Management-System';
+  ifRegisted : boolean =true;
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,55 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './healthOfficer/sidebar/dashboard/dashboard.component';
+import { AppointmentsComponent } from './healthOfficer/sidebar/appointments/appointments.component';
+import { RecordInjuriesComponent } from './healthOfficer/sidebar/record-injuries/record-injuries.component';
+import { PlayersHealthRecordsComponent } from './healthOfficer/sidebar/players-health-records/players-health-records.component';
+import { BlogsComponent } from './healthOfficer/sidebar/blogs/blogs.component';
+import { SafetyAdviceComponent } from './healthOfficer/sidebar/safety-advice/safety-advice.component';
+import { SettingsComponent } from './healthOfficer/sidebar/settings/settings.component';
+import { SignOutComponent } from './healthOfficer/sidebar/sign-out/sign-out.component';
+import { SafetyAdviceCreateComponent } from './healthOfficer/sidebar/safety-advice-create/safety-advice-create.component';
+ 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : '',
+        redirectTo : 'dashboard',
+        pathMatch : 'full'
+    },
+   {
+    path : 'dashboard',
+    component : DashboardComponent
+   },
+   {
+    path : 'appointments',
+    component : AppointmentsComponent
+   },
+  {
+    path : 'recordInjuries',
+    component : RecordInjuriesComponent
+  },
+  {
+    path : 'playersHealthRecords',
+    component : PlayersHealthRecordsComponent
+  },
+  {
+    path : 'blogs',
+    component : BlogsComponent
+  },
+  {
+    path : 'safetyAdvice',
+    component : SafetyAdviceComponent
+  },
+  {
+    path : 'settings',
+    component : SettingsComponent
+  },
+  {
+    path : 'signOut',
+    component : SignOutComponent
+  },
+  {
+    path : 'safetyAdvice-create',
+    component : SafetyAdviceCreateComponent
+  } 
+];
