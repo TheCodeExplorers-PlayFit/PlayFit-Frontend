4bb0c88539b384076ee4564147957b33fa8a2cf7
Nethmi Ayeshika <ayeshikakpn.22@uom.lk>
2025-04-07 17:38:26 +0530

    coach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-root',
+  standalone: true,
+  imports: [SidebarComponent, NavbarComponent,RouterOutlet], // **Add RouterModule to imports**
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  title = 'Sports-Management-System';
+  title = 'frontendofcoach';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { BookingHistoryComponent } from './booking-history/booking-history.component';
+import { ScheduleSelectorComponent } from './schedule-selector/schedule-selector.component';
+import { StadiumListComponent } from './stadium-list/stadium-list.component';
+import { StadiumDetailComponent } from './stadium-detail/stadium-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PrivateRequestsComponent } from './private-requests/private-requests.component';
+import { ComplaintsComponent } from './complaints/complaints.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'booking-history', component: BookingHistoryComponent },
+    { path: 'schedule-selector', component: ScheduleSelectorComponent },
+    { path: 'stadium-list', component: StadiumListComponent },
+    { path: 'stadium-detail', component: StadiumDetailComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'private-playing-requests', component: PrivateRequestsComponent },
+    { path: 'complaints', component: ComplaintsComponent },
+
+
+
+];
+
+
