2e9764507d94825376632545e45728e125dc8186
Nethmi Ayeshika <ayeshikakpn.22@uom.lk>
2025-05-24 18:38:41 +0530

    salry report pdf

--- src/app/services/coach-salary/coach-salary.service.ts
+++ src/app/services/coach-salary/coach-salary.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 export interface CoachSalary {
   coach_id: number;
@@ -15,22 +14,15 @@ export interface CoachSalary {
 export class CoachSalaryService {
   private apiUrl = 'http://localhost:5000/api/coach-sessions/salaries';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getCoachSalaries(): Observable<CoachSalary[]> {
     const token = localStorage.getItem('token');
     console.log('Token in CoachSalaryService:', token);
-
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       ...(token ? { Authorization: `Bearer ${token}` } : {})
     });
-
-    return this.http.get<CoachSalary[]>(this.apiUrl, { headers }).pipe(
-      map(data => {
-        console.log('Raw API response:', data);
-        return data.length > 0 ? [data[0]] : [];
-      })
-    );
+    return this.http.get<CoachSalary[]>(this.apiUrl, { headers });
   }
 }
