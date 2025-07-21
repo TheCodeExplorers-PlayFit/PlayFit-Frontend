89584a936891aea7397ed503b7967e5d48790776
Nilupulee1 <geethmanilupuli@gmail.com>
2025-07-21 20:58:20 +0530

    updated

--- src/app/models/maintenance-requests.ts
+++ src/app/models/maintenance-requests.ts
@@ -1,12 +1,20 @@
 
 export interface Complaint {
   id: number;
-  description: string;
+  reported_by: number;
   reported_by_name: string;
-  stadium_name?: string;
-  created_at: string | Date;
+  reported_to: string;
+  stadium_id: number;
+  stadium_name: string;
+  owner_id: number;
+  owner_name: string;
+  owner_email: string;
+  coach_id?: number;
+  description: string;
   status: 'pending' | 'resolved' | 'in_progress';
+  created_at: string;
 }
+
 
 export interface Card {
   subtitle: string;
